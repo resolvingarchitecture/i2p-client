9a9b57118c5be46e99e0cc47314263b8585db91f
objectorange <brian@resolvingarchitecture.io>
2020-03-24 18:13:31 -0700

    begin impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+use ra_common::models::{Network, Packet};
+
+struct I2PClient {
+
+}
+
+impl I2PClient {
+    pub fn new() -> Box<I2PClient> {
+        Box::new(I2PClient {
+
+        })
+    }
+    pub fn init(&mut self) {
+
+    }
+}
+
+impl Network for I2PClient {
+    fn handle(&mut self, packet: &mut Packet) {
+        unimplemented!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
