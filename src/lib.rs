6e4929f1e22b161f151e0d3be2e33a241b2a99d2
objectorange <brian@resolvingarchitecture.io>
2020-03-25 14:54:46 -0700

    remove all i2p libs for now - awaiting on i2p and ire rust crates to mature

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
-use log::{trace,info,warn};
+
+use log::{debug,trace,info,warn};
+
 use ra_common::models::{Network, Packet};
-use i2p::sam::{DEFAULT_API,SamConnection,Session,SessionStyle};
-// use crate::sam::{DEFAULT_API,SamConnection,Session,SessionStyle};
-// pub mod sam;
-// pub mod parsers;
+
+pub enum ClientType {
+    Local    = 0,
+    Embedded = 1,
+}
 
 pub struct I2PClient {
 
@@ -15,26 +18,32 @@ impl I2PClient {
     }
     pub fn init(&mut self) {
         info!("{}","Initializing I2P Client...");
-        match SamConnection::connect(DEFAULT_API) {
-            Ok(mut conn) => {
-                info!("dest: {}",conn.naming_lookup("1m5.i2p").unwrap());
-            },
-            Err(e) => {
-                warn!("Error: {}",e.to_string());
-            }
-        }
+        // match SamConnection::connect("127.0.0.1:7656") {
+        //     Ok(mut conn) => {
+        //         let dest = conn.naming_lookup("1m5.i2p").unwrap();
+        //         info!("dest: {}",dest);
+        //     },
+        //     Err(e) => {
+        //         warn!("Error: {}",e.to_string());
+        //     }
+        // }
         // match Session::create(DEFAULT_API,
-        //                       "PwyBbml~AOWQsrZ61y2UqdXSuGGy5E6whh93k1dzIuvBRWr5K09X3Pzpt1wBFffA8Y02vAyrX8d2rCjrRcYFaxhbkummyUqeN7zdeqqZ3NDbbm-qyZH7tBEv-QwjxA18hxG~x~9-tP-ixiYMNDOMs5FhPThZkb-RFpY1HgTB19DbGL35KqYWtmuZG1~ZYy99c~u3kDEOroj1Jm5vAQFXbemlUOLrUZkyV4b0UkLqe1KRMWSOdGb8QL6PraBLx3QFvVJfrEji~u~8ztEIOZovvy9xOf52SvTsUQc-OVaFU7xPnvbxMlnW43JgT9w0RG2~Rh6UhKvmGcnUb5yVhdmujXY9gJ98bJ--VhBBAzGbPBxPN0~WqQ2rzEQ7x-~A2SBGhYfRhxQTFiC-n~cQehok6zMEcm0gSsD80mYdZWFskymm-OqXTd~AxoXkuX6xdv3l2TYJz6TEl-5TvTw38v7LD8WeDiC001YCwGKenxc4VQfi2xzXW9hAuoTuPww8L5yvBQAEAAcAAA==",
         //                       "1m5.i2p",
+        //                       "1m5",
         //                       SessionStyle::Datagram) {
         //     Ok(session) => {
-        //         let session = Box::new(session);
         //         info!("IP: {}",session.sam_api().unwrap().ip().to_string())
         //     },
         //     Err(err) => {
         //         warn!("Error: {}",err.to_string());
         //     }
         // }
+        // let mut connection = StreamConnect::new(DEFAULT_STREAM_API, "1m5.i2p", 8000, "1m5").unwrap();
+        // let local_addr = connection.local_addr().unwrap();
+        // let peer_addr = connection.peer_addr().unwrap();
+        // info!("Local addr: {}:{}",local_addr.0,local_addr.1);
+        // info!("Peer addr: {}:{}",peer_addr.0,peer_addr.1);
+
     }
 }
 
