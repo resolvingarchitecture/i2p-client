70fa83ab9cb5f61cdd59b9f7fbc3ce76ce556595
objectorange <brian@resolvingarchitecture.io>
2020-04-07 18:19:55 -0700

    Moving some code over from i2p-rs project as it appears to be a dead project; getting INVALID_KEY session error - no idea what parameter 'nickname' refers to.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,16 +3,29 @@ extern crate simple_logger;
 
 use log::{trace,info};
 use i2p_client::I2PClient;
-use ra_common::models::{Envelope, Route};
+use ra_common::models::{Envelope, Route, Packet, PacketType, NetworkId};
+use nom::tag_cl;
 
 fn main() {
     simple_logger::init().unwrap();
     trace!("Starting I2P Client Daemon...");
-    let mut client = I2PClient::new();
-    client.init();
-    let msg = format!("{}","Messge from 1 to 2");
+    let mut client_1 = I2PClient::new();
+    match client_1.init(true) {
+        Ok(addr_1) => trace!("address 1: {}",addr_1.as_str()),
+        Err(e) => trace!("{}",e)
+    }
+
+    let mut client_2 = I2PClient::new();
+    let addr_2 = client_2.init(false);
+    match client_1.init(true) {
+        Ok(addr_2) => trace!("address 2: {}",addr_2.as_str()),
+        Err(e) => trace!("{}",e)
+    }
+
+    let msg = format!("{}","Message from 1 to 2");
     let msg = msg.into_bytes();
     let env = Envelope::new(1, 2, msg);
+    client_1.send(env);
 
     trace!("I2P Client Daemon Stopped.");
 }
